4d46c994a2dfe690122bf4a298174acaefd2ab87
ywyp08 <stepa.spilka@gmail.com>
2025-11-15 13:06:20 +0400

    fibonacci

---
+++ 03_fibonacci/main.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+int fibonacci(int n)
+{
+	if (n == 0)
+		return 0;
+	if (n == 1)
+		return 1;
+	return fibonacci(n-1) + fibonacci(n-2);
+}
+
+int main(int argc, char **argv)
+{
+	int n;
+
+	n = std::stoi(argv[1]);
+	std::cout << fibonacci(n) << "\n";
+}
