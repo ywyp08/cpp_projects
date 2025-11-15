c84ea3c7f707f15f5b2bf7f41528f4c9015df184
ywyp08 <stepa.spilka@gmail.com>
2025-11-15 13:06:07 +0400

    structure change

--- 02_count_to_n/main.cpp
+++ 02_count_to_n/main.cpp
@@ -5,22 +5,14 @@ int main(int argc, char **argv)
 	int n;
 	int x;
 
-	if (argc == 1)
-	{
-		std::cerr << "Enter a number\n";
-		return -1;
-	}
-	if (argc > 2)
-	{
-		std::cerr << "Enter one number\n";
-		return -1;
-	}
-	if (argc == 2)
-	{
+	if (argc == 2) {
 		n = 0;
 		x = std::stoi(argv[1]);
 		while (x > n)
 			std::cout << n++ << "\n";
+		return 0;
+	} else {
+		std::cerr << "Enter exactly one argument\n";
+		return 1;
 	}
-	return 0;
 }
